e0687a774e02fa402edbbc92c56ceae2b2815591
poornima <pbatham21@gmail.com>
2023-06-22 18:12:46 +0530

    with inner join sql queries

--- myproject/start/routes.ts
+++ myproject/start/routes.ts
@@ -29,6 +29,15 @@ Route.get('/', async () => {
 // .table('employee')
 // .insert({ fname: 'virk',lname:'kaushak',password:76767, email: 'virk@adonisjs.com',phone:768768 })
 
+
+
+
+// const users = await Database
+//   .from('employee') // 👈 gives an instance of select query builder
+//   .select('*')
+// .timeout(5000)
+// return users
+
 // FOR DATABASE  QUERIES
 // const query = Database.connection()
 // console.log(query);
@@ -38,17 +47,17 @@ Route.get('/', async () => {
 // const query2 =  Database.from('employee')
 // .where((query)=>{
 //   query
-  // .where('fname','virk').where('empid','55').where('lname','kaushal')
-    // .where('fname','virk')
+//   .where('fname','poornima').where('empid','1').where('lname','batham')
 
 
 // })
-// .orWhere((query) => {
+//  .orWhere((query) => {
 //   query
 //     .where('email', 'vk@adonisjs.com')
 
 // })
 // return query2
+
 
 
 
@@ -76,10 +85,116 @@ Route.get('/', async () => {
 
 
   
+  // await Database
+  // .from('employee')
+  // .where('empid', 68)
+  // .update({fname:'hgkjh',lname:'kiys',password:98877860,email:'oi@gmail.com',phone:7676878})
+
+
+// const users = await Database
+  // .from('employee') // 👈 gives an instance of select query builder
+  // .select('empid','fname')   acess the only specifi columns
+  //  return users.
+
+
+  // const adults = await Database
+  // .from('employee')
+  // .where((query) => {
+  //     query
+  // .where('password', '=', 798)
+  // })
+  // .orWhere((query) => {
+  //     query
+  //     .where('phone',768768)
+        
+  //   })
+  //   return 
+  
+
+//   const b = await Database
+//   .from('employee')
+//   .whereIn('empid', [1,69,70,71,72])
+
+// return b
+
+
+
+// const v = await Database
+// .from('employee')
+//   .whereLike('fname', '%poornima%')
+  
+//   return v
+
+
+
+  // const adult = await Database
+  // .from('employee')
+  // .where((query) => {
+  //   query
+  //     .where('fname','poornima')
+  // })
+  // .andWhere((query) => {
+  //   query
+  //     .where('email', 'rk@ds.com')
+      
+  // })
+  // .orWhere((query) => {
+  //   query
+  //     .where('password', 798)
+      
+  // })
+  // return adult
+ 
+
+
+
+
+
+
+
+
+
+
+
   await Database
+  .table('employee')
+  .multiInsert([{
+      fname: 'virk',
+      lname:'jan',
+      password: 8098,
+      email: 'virk@adonisjs.com',
+      phone:8768
+    },
+    {
+      fname: 'sonu',
+      lname:'gupta',
+      password: 8098,
+      email: 'sonu@adonisjs.com',
+      phone:8768
+    }
+  
+  ])
+ 
+
+  
+
+
+
+
+
+
+
+  const adult = await  Database
   .from('employee')
-  .where('empid', 68)
-  .update({fname:'hgkjh',lname:'kiys',password:98877860,email:'oi@gmail.com',phone:7676878})
+  .join('emplyee1', 'emplyee1.ref_id', '=', 'employee.empid')
+  // .select('*')
+  .select('employee')
+  
+
+
+  return adult
+
+
 
 })
 
