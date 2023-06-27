1ae4c958f8a073f07b0e33927d9f33f4fa9ee99d
ubitechdevelopers <ubitechdeveloper@ubitechsolutions.com>
2023-06-27 17:12:54 +0530

    old code

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,8 +1,9 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 // import Validator from  'App/Validators/User1Validator'
 import User2 from 'App/Models/User2';
+import User1Validator from 'App/Validators/User1Validator';
 // import Database from '@ioc:Adonis/Lucid/Database'
-import { schema, rules } from '@ioc:Adonis/Core/Validator'
+// import { schema, rules } from '@ioc:Adonis/Core/Validator'
 // import User2 from 'App/Models/User2';
 
 export default class UsersController  {
@@ -22,61 +23,69 @@ export default class UsersController  {
 
 
 
-
-
-
-
-    const postSchema = schema.create({
-     
-    // empid: schema.number(),
-    fname: schema.string.optional([
-      rules.alpha()
-    ]
-
-      
-    ),
-    lname: schema.string([
-      rules.alpha()
-    ]),
+  const payload: any = await request.validate(User1Validator. postSchema, )
+  console.log(payload)
   
-    
-    password: schema.string([
-      rules.minLength(4),rules.minLength(2)
-    ]) , email: schema.string([
-      rules.email(),rules.unique({table:'employee',column:'email'})
-
-
-    ]),  phone:schema.string([rules.mobile(),rules.maxLength(10),rules.minLength(10)]),
-    })
-    
-
-    
-
-    
-    const messages = {
-  '*':(field, rule) => {
-     return `${rule} validation error on ${field}`
-    },
-      
-      required:'{{field}} is required',
-      unique:'{{field}} must be unique',
-      maxLength:'The length is too long',
-      minLength:'The length is too short',
-      email:'email should be  in @ format',
-      mobile:'mobile containes only 10 numbers',
-    
-      //unique:'{{field}} must be unique',
-  //     minLength: '{{field}} must be atleast {{options.minLength}} characters long',
-  //     maxLength:'{{field}} cannot be longer than {{options.maxLength}} characters long',
-
-   }
-  
-  const payload: any = await request.validate({ schema: postSchema,messages })
+ 
   const post: User2 = await User2.create(payload)
 
   return response.ok(post)
 
     await post.save()
+
+
+
+    // const postSchema = schema.create({
+     
+    // // empid: schema.number(),
+    // fname: schema.string.optional([
+    //   rules.alpha()
+    // ]
+
+      
+    // ),
+    // lname: schema.string([
+    //   rules.alpha()
+    // ]),
+  
+    
+    // password: schema.string([
+    //   rules.minLength(4),rules.minLength(2)
+    // ]) , email: schema.string([
+    //   rules.email(),rules.unique({table:'employee',column:'email'})
+
+
+    // ]),  phone:schema.string([rules.mobile(),rules.maxLength(10),rules.minLength(10)]),
+    // })
+    
+
+    
+
+    
+  //   const messages = {
+  // '*':(field, rule) => {
+  //    return `${rule} validation error on ${field}`
+  //   },
+      
+  //     required:'{{field}} is required',
+  //     unique:'{{field}} must be unique',
+  //     maxLength:'The length is too long',
+  //     minLength:'The length is too short',
+  //     email:'email should be  in @ format',
+  //     mobile:'mobile containes only 10 numbers',
+    
+  //     //unique:'{{field}} must be unique',
+  // //     minLength: '{{field}} must be atleast {{options.minLength}} characters long',
+  // //     maxLength:'{{field}} cannot be longer than {{options.maxLength}} characters long',
+
+  //  }
+  
+  // const payload: any = await request.validate({ schema: postSchema,messages })
+  // const post: User2 = await User2.create(payload)
+
+  // return response.ok(post)
+
+  //   await post.save()
 
 
 
@@ -117,10 +126,10 @@ export default class UsersController  {
 //   .from('employee') // 👈 gives an instance of select query builder
 //   .select('*')
 // return users
-const user = await User2.all()
+//const user = await User2.all()
 // const user2 = await User2.findBy('fname', 'poornimabatham')
 // return user2
-return user
+
   }
 
   public async edit({}: HttpContextContract) {
@@ -130,5 +139,8 @@ return user
 
   public async update({}: HttpContextContract) {}
 
-  public async destroy({}: HttpContextContract) {}
+//   public async destroy({}: HttpContextContract) {
+//     const d = await User2.find(1)
+// await d.delete()
+//   }
 }
